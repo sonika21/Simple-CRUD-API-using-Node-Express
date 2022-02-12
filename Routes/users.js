0089fdd8b59c11ef274e28d25ca67e579376ef88
Sonika Panigrahi <sonika.panigrahi@gmail.com>
2022-02-12 08:55:05 +0530

    Initial Commit- Version 1

--- Routes/users.js
+++ Routes/users.js
@@ -1,1 +1,20 @@
+import express from 'express'
 
+
+import { createUser, getUsers, getUser, deleteUser, updateUser } from '../controllers/users.js';
+
+const router = express.Router() //initialize our router
+
+//all routes here are starting with /users
+router.get('/', getUsers)
+
+router.post('/', createUser)
+
+// /user/2 -- retrive this value in callback => req.params{id: 2}
+router.get('/:id', getUser )
+
+router.delete('/:id',deleteUser)
+
+router.patch('/:id',updateUser)
+
+export default router
